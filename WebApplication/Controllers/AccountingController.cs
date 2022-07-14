8c74f0db57a05c7198c1ec1cb0631aeecdd7c360
CDani11 <cdanitg@gmail.com>
2022-07-14 10:24:49 -0500

    Add project files.

---
+++ WebApplication/Controllers/AccountingController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class AccountingController : Controller
+    {
+        private SqlConnection conn;
+        private SqlCommand cmd;
+        private SqlDataReader rdr;
+
+        // GET: Accounting
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetProducts()
+        {
+            string msg = string.Empty;
+            IDictionary<int, string> dicProducts = new Dictionary<int, string>();
+
+            try
+            {
+                conn = new SqlConnection(ConfigurationManager.ConnectionStrings["dbWebApp"].ConnectionString);
+
+                conn.Open();
+
+                cmd = new SqlCommand("dbo.SPConsultProduct", conn)
+                {
+                    CommandType = System.Data.CommandType.StoredProcedure
+                };
+
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    int id = (int)rdr["ID"];
+                    string description = rdr["Description"].ToString();
+
+                    dicProducts.Add(id, description);
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+
+            return Json(new { data = dicProducts.ToList(), message = msg }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplication/Scripts/Accounting/Index.js
@@ -0,0 +1,80 @@
+﻿(function (w, d, $) {
+  'use strict';
+
+  let $tblInvoice;
+
+  $(function () {
+    $('.calculateTotal').on('change', calculateTotal);
+
+    $('#btnSave').on('click', createRow);
+
+    consultProducts();
+  });
+
+  const consultProducts = function () {
+    $.get('/Accounting/GetProducts')
+      .done(function (resp) {
+        let data = resp.data;
+        let message = resp.message;
+
+        if (typeof data != 'object')
+          return;
+
+        let tag;
+
+        for (let { Key, Value } of data) {
+          tag += `<option value='${Key}'>${Value}</option>`;
+        }
+
+        $('#cmbProducts').append(tag);
+
+        console.log(data);
+      })
+      .fail();
+  };
+
+  const calculateTotal = function () {
+    let quantity = parseInt($('#NumQuantity').val());
+    let price = parseFloat($('#NumPrice').val());
+
+    let total = quantity * price;
+
+    $('#NumTotal').val(total);
+  };
+
+  const createRow = function () {
+    let data = {
+      IdProduct: $('#cmbProducts').val(),
+      Product: $('#cmbProducts option:selected').text(),
+      Quantity: $('#NumQuantity').val(),
+      Price: $('#NumPrice').val(),
+      Total: $('#NumTotal').val()
+    };
+
+    if ($tblInvoice)
+      $tblInvoice
+        .row
+        .add(data)
+        .draw();
+
+    $('#mdlRegister').modal('hide');
+  };
+
+  $tblInvoice = $('#tblInvoice').DataTable({
+    columns: [
+      { data: 'IdProduct' },
+      { data: 'Product' },
+      { data: 'Quantity' },
+      { data: 'Price' },
+      { data: 'Total' },
+      {
+        data: null,
+        defaultContent: `
+          <div class='d-flex justify-content-around'>
+            <button class='btn btn-circle btn btn-danger'>Eliminar</button>
+          </div>`
+      }
+    ]
+  });
+
+})(window, document, jQuery);
